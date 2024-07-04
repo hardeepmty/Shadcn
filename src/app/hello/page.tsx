560407c20d1e1579af086c4e8f0093e5146f472b
HARDEEP MOHANTY <mohanty4raj@gmail.com>
2024-07-05 01:40:18 +0530

    done

---
+++ src/app/hello/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+function page() {
+  return (
+    <main className='h-full flex justify-center items-center flex-col'>
+      <div>
+        My Page
+      </div>
+      <Button variant="default">Shadcn Button</Button>
+    </main>
+  )
+}
+
+export default page
